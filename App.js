8ead34ed2f3b132e5b2a71d1dbc5bf50b331f67e
alestor123 <alestoraldous@gmail.com>
2020-11-17 15:31:39 +0530

    added github redirct

--- App.js
+++ App.js
@@ -3,10 +3,15 @@ require('dotenv').config()
 var express = require('express'),
 app = express(),
 argv = process.argv[2],
+pck = require('./package.json'),  
 port = process.env.PORT || argv || 3000;
 
 app.get('/', (req, res) => {
     res.json({'info':'This is the version badge'})
+})
+
+app.get('/github', (req, res) => {
+    res.redirect(pck.homepage)
 })
 
 app.listen(port, () => console.log(`server running at ${port}`))
