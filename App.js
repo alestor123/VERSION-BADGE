98e88227c22edb283b6eb6ef1b2bddce107e192e
alestor123 <alestoraldous@gmail.com>
2020-11-17 15:28:31 +0530

    added home route

--- App.js
+++ App.js
@@ -5,5 +5,8 @@ app = express(),
 argv = process.argv[2],
 port = process.env.PORT || argv || 3000;
 
+app.get('/', (req, res) => {
+    res.json({'info':'This is the version badge'})
+})
 
 app.listen(port, () => console.log(`server running at ${port}`))
